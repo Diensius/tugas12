ec5c48b1be95419b9d8a71e4cefbb1b70b7808f0
Diensius <laptopasus282@gmail.com>
2022-02-22 21:40:40 +0700

    first commit

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <title>Tugas #12 Laravel Intro</title>
+    </head>
+
+    <body>
+        <h1>SanberBook</h1>
+        <h2>Social Media Developer Santai Berkualitas</h2>
+        <p>Belajar dan Berbagi agar hidup ini semakin santai berkualitas</p>
+        <h3><b>Benefit Join di SanberBook</b></h3>
+        <ul>
+            <li>Mendapatkan motivasi dari sesama developer</li>
+            <li>Sharing knowledge dari para mastah Sanber</li>
+            <li>Dibuat oleh calon web developer terbaik</li>
+        </ul>
+        <h3><b>Benefit Join di SanberBook</b></h3>
+        <ol>
+            <li>Mengunjungi website ini</li>
+            <li>Mendaftar di <a href="/register">Form Sign Up</a></li>
+            <li>Selesai!</li>
+        </ol>
+    </body>
+</html>
---
+++ resources/views/welcome/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <title>Tugas #12 Laravel Intro</title>
+    </head>
+
+    <body>
+        <h1>SELAMAT DATANG {{$namaDepan}} {{$namaBelakang}} !</h1> <!-- cara menampilkan data di php laravel -->
+        <h2>Terimakasih telah bergabung di Sanberbook. Social media kita bersama!</h2>
+        <a href="/"><button type="button">Home</button></a>
+    </body>
+</html>
